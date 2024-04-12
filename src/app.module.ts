8ed8a12508fbb2721a1252abaf57aa3e2a1c55ef
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-04-12 16:36:54 +0300

    create resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TodoModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1111',
+      database: 'auth',
+      autoLoadEntities: true,
+      synchronize: true,
+      logger: 'debug',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
